0048ac22e291204a87cf3193dbe1815ce8597019
Alex Scarlett <alex.scarlett91@gmail.com>
2019-07-27 17:23:44 -0400

    Mapped over the array of response objects for the images and created an ImageCard component for each image

--- src/components/ImageList.tsx
+++ src/components/ImageList.tsx
@@ -1,13 +1,18 @@
+import './ImageList.css';
 import React from 'react';
 import { PhotoResponse } from '../utils/PhotoResponse';
+import ImageCard from './ImageCard';
 
 interface ImageListProps {
   images: PhotoResponse[];
 }
 
 const ImageList = (props: ImageListProps) => {
-  console.log(props.images);
-  return <div>ImageList</div>;
+  const images = props.images.map(image => {
+    return <ImageCard key={image.id} image={image} />;
+  });
+
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
