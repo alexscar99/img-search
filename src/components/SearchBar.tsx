ff42e08a1ff5ba37c6039fea1944d047727c71c9
Alex Scarlett <alex.scarlett91@gmail.com>
2019-07-27 01:40:07 -0400

    Started wiring up 'SearchBar' component with 'onChange' event handler for form input

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+interface SearchBarState {
+  term: string;
+}
+
+class SearchBar extends React.Component<{}, SearchBarState> {
+  state = { term: '' };
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={e => this.setState({ term: e.target.value })}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
